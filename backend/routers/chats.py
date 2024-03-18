d27580105c5a1ed03df5969d2b79f7d3389829c0
singark21 <arkein.singh@hotmail.com~>
2024-03-17 20:52:58 -0600

    updated status code

--- backend/routers/chats.py
+++ backend/routers/chats.py
@@ -26,6 +26,7 @@ chats_router = APIRouter(prefix="/chats", tags=["Chats"])
     "/{chat_id}/messages",
     response_model=MessageResponse,
     description="Creates a new message for chat with the given chat id.",
+    status_code=201
 )
 def post_msg(new_message: NewMessage, chat_id= int, user: UserInDB = Depends(get_current_user),  session: Session = Depends(db.get_session)):
     
