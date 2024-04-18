51b7ac5e67a91b4ffb97a6cc9dab3b0e6a26f816
singark21 <arkein.singh@hotmail.com~>
2024-04-18 13:48:35 -0600

    start of frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,7 @@ import Registration from "./components/Registration";
 import TopNav from "./components/TopNav";
 
 
+const baseUrl = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000";
 
 
 const queryClient = new QueryClient();
